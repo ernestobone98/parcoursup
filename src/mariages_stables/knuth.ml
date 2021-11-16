40339a2ac6987eeef5efe19eb8d2726c50c99b21
nadjibkhammar <khammarnadjib@yahoo.fr>
2021-11-16 23:02:59 +0100

    un test qui bloque

--- src/mariages_stables/knuth.ml
+++ src/mariages_stables/knuth.ml
@@ -1,27 +1,47 @@
-open Definitions 
+open Definitions
 
-let algo ?(affiche_config=false) entree = 
-  if (entree_valide entree) then begin 
+let choix x = function
+  | (-1) -> None
+  | _ -> Some x
+
+let algo ?(affiche_config=true) entree =
+  if (entree_valide entree) then begin
     ignore affiche_config;
-    let (k : int ref)  = ref 0 in
-    let conf = {rang_appel_de = Array.make entree.n 0 ; fiance_de = Array.make entree.n  None} in 
+    let k = ref 0 in
+    let h: homme ref = ref 0 in
+    let f: femme ref = ref 0 in
+    (* let choix = ref (-1) in *)
+    let conf : configuration = {rang_appel_de = Array.make entree.n 0 ; fiance_de = Array.make entree.n None } in 
+    let p : (femme*femme) array = Array.make entree.n (0,0) in
     while !k < entree.n do
-      let h : (homme ref) = ref !k in
-      let f = ref (entree.liste_appel_de.(!h).(conf.rang_appel_de.(!h))) in
-      while conf.rang_appel_de.(!h) < entree.n do
+      while (choix !h) <> (choix (-1)) do
+        f := entree.liste_appel_de.(!h).(conf.rang_appel_de.(!h));
         if conf.fiance_de.(!f) = None then begin
-          (* Casar a la mujer actual - Marier a la femme actuelle *)
-          conf.fiance_de.(!f) <- Some !k;
-        end
+          conf.fiance_de.(!f) <- Some !h;
+          h:= (-1) ;
+          print_configuration conf;
 
-        else if entree.prefere.(!f) !h conf.fiance_de.(!f)
-          (*Mirar si la mujer lo prefiere
-          Voir si la femme actuelle le prefere 
-          *)
-         then conf.fiance_de.(!f) <- Some !k
+          (*h := !h + 1;*)
+        end
+        else if entree.prefere.(!f) !h (Option.get conf.fiance_de.(!f))
+        then begin 
+          let h_tmp = Option.get conf.fiance_de.(!f) in
+          conf.fiance_de.(!f) <- Some !h;
+          h := h_tmp;
+          conf.rang_appel_de.(!h) <- conf.rang_appel_de.(!h) + 1; (* on peut le mettre au debut car le rang n'affecte pas l'homme*)
+        end else
+          conf.rang_appel_de.(!h) <- conf.rang_appel_de.(!h) + 1;
+          print_configuration conf;
       done;
+      p.(!k) <- (!h,conf.rang_appel_de.(!h)); 
       k := !k + 1;
     done;
+    (* let p : (femme*femme) array = [||] in
+    for i=0 to entree.n do
+      p.(i) <- (i,conf.rang_appel_de.(i))
+    done; *)
+    
+    Array.to_list p;
   end
   else invalid_arg "L'entree n'est pas valide"
 
