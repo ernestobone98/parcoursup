1dfd5b86c1c850d320af36488a7f32da8d4356ac
ernestobone98 <ernesto.bone@outlook.com>
2021-11-20 16:48:45 +0100

    eureka

--- src/mariages_stables/knuth.ml
+++ src/mariages_stables/knuth.ml
@@ -1,70 +1,39 @@
 open Definitions
 
-let algo ?(affiche_config=false) entree =
-  if (entree_valide entree) then begin 
-    ignore affiche_config;
-    let k = ref 0 in
-    let tmp = ref 0 in
-    let conf = {rang_appel_de = Array.make entree.n 0 ; fiance_de = Array.make entree.n None } in
-    let f = ref (entree.liste_appel_de.(!k).(conf.rang_appel_de.(!k))) in
-    (*let next = ref 0 in*)
-    let p : (homme*femme) array = Array.make entree.n (0,0) in 
-    while !k < entree.n do 
-      let quite_loop = ref false in
-      f := entree.liste_appel_de.(!k).(conf.rang_appel_de.(!k));
-      Format.printf "Permiere boucle : \nk = %d f = %d\n" !k !f;
-      if conf.fiance_de.(!f) = None then begin
-        conf.fiance_de.(!f) <- Some !k;
-        Format.printf "f ete celib donc on marie : \nk = %d f = %d\n" !k !f;
-      end
-      else while( conf.rang_appel_de.(!k) < entree.n && not !quite_loop) do
-          if entree.prefere.(!f) !k (Option.get conf.fiance_de.(!f)) then begin
-            tmp := Option.get conf.fiance_de.(!f);
-            conf.fiance_de.(!f) <- Some !k;
-            p.(!k) <- (!k,conf.rang_appel_de.(!k));
-            Format.printf "Si f prefere celui qui propose et non actuel donc on marie : \nk = %d f = %d\n" !k !f;
-            k:= !tmp;
-            conf.rang_appel_de.(!k) <- conf.rang_appel_de.(!k) + 1;
-            (* k ne propose pas a la femme suivante*)
-            quite_loop := true;
-          end
-          else begin
-            conf.rang_appel_de.(!k) <- conf.rang_appel_de.(!k) + 1;
-            quite_loop := true;
-            Format.printf "Si f prefere actuel on marie pas. On pase a lhomme suivant : \nk = %d f = %d\n" !k !f;
-          end
-        done;
-      if !quite_loop = false then begin
-        p.(!k) <- (!k,conf.rang_appel_de.(!k)); 
-        k := !k + 1;
+let algo ?(affiche_config=true) entree =
+  if (entree_valide entree) then begin
+  ignore affiche_config;
+  let k = ref 0 in
+  let h: homme ref = ref 0 in
+  let f: femme ref = ref 0 in
+  let conf : configuration = {rang_appel_de = Array.make entree.n 0 ; fiance_de = Array.make entree.n None } in 
+  let p : (homme*femme) array = Array.make entree.n (0,0) in
+  while !k < entree.n do
+    h:=!k;
+    while !h <> -1 do
+      f := entree.liste_appel_de.(!h).(conf.rang_appel_de.(!h));
+      if conf.fiance_de.(!f) = None then 
+        begin conf.fiance_de.(!f) <- Some !h;
+          p.(!h) <- (!h,!f);
+          print_configuration conf;
+          h:= (-1) ;
+        end 
+      else if entree.prefere.(!f) !h (Option.get conf.fiance_de.(!f))
+      then begin 
+        let h_tmp = Option.get conf.fiance_de.(!f) in
+        conf.fiance_de.(!f) <- Some !h;
+        p.(!h) <- (!h,!f);
+        h := h_tmp;
+        conf.rang_appel_de.(!h) <- conf.rang_appel_de.(!h) + 1; 
+        print_configuration conf;
+      end 
+      else begin
+        conf.rang_appel_de.(!h) <- conf.rang_appel_de.(!h) + 1;
+        print_configuration conf;
       end
     done;
-    Array.to_list p;
+    k := !k + 1;
+  done; 
+  Array.to_list p;
   end
   else invalid_arg "L'entree n'est pas valide"
-
-(* let algo ?(affiche_config=false) entree =
-  if (entree_valide entree) then begin 
-    ignore affiche_config;
-    let k = ref 0 in
-    let conf = {rang_appel_de = Array.make entree.n 0 ; fiance_de = Array.make entree.n None } in 
-    while !k < entree.n do
-      let h = ref !k in
-      let f = ref (entree.liste_appel_de.(!h).(conf.rang_appel_de.(!h))) in
-      while conf.rang_appel_de.(!h) < entree.n do
-        if conf.fiance_de.(!f) = None then begin
-          conf.fiance_de.(!f) <- Some !k 
-        end
-        else if entree.prefere.(!f) !k (Option.get conf.fiance_de.(!f))
-        then conf.fiance_de.(!f) <- Some !k
-      done;
-      k := !k + 1;
-    done;
-  end
-  else invalid_arg "L'entree n'est pas valide" *)
-
-
-(* let algo ?(affiche_config=false) entree =
-  ignore affiche_config;
-  ignore entree;
-  failwith "non implémenté" *)
