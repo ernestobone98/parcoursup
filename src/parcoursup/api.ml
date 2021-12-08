7eea916f6f219a7bdd1e69ba02fce1973a38b061
nadjibkhammar <khammarnadjib@yahoo.fr>
2021-12-08 23:48:58 +0100

    Version finale

--- src/parcoursup/api.ml
+++ src/parcoursup/api.ml
@@ -57,97 +57,32 @@ let ajoute_voeu session ~rang_repondeur ~nom_candidat ~nom_formation =
       session.voeu <- [hd] @ tl ; aux tl 
     in aux session.voeu 
 
-    (* let ajoute_voeu session ~rang_repondeur ~nom_candidat ~nom_formation = 
-      let rec aux (v: voeu list) = match v with 
-      | [] -> session.voeu <- [{rang_repondeur=rang_repondeur ;nom_candidat=nom_candidat ; nom_formation = nom_formation }]
-      | [x] when x.nom_candidat = nom_candidat && x.nom_formation = nom_formation -> 
-          session.voeu <- [{rang_repondeur=rang_repondeur ;nom_candidat=nom_candidat ; nom_formation = nom_formation }]
-      | hd::tl -> 
-          aux tl ;
-          session.voeu <- [hd] @ tl
-        in aux session.voeu  *)
+let ajoute_commission session ~nom_formation ~fonction_comparaison = 
+  ignore session;
+  ignore nom_formation;
+  ignore fonction_comparaison;
+  failwith "non implémenté"
 
+let reunit_commissions session =
+  ignore session;
+  failwith "non implémenté"
 
+let nouveau_jour session =
+  ignore session;
+  failwith "non implémenté"
 
-    (* let ajoute_voeu session ~rang_repondeur ~nom_candidat ~nom_formation = 
-      let rec aux (v: voeu list) = match v with 
-      | [] -> [{rang_repondeur=rang_repondeur ;nom_candidat=nom_candidat ; nom_formation = nom_formation }]
-      | [x] when x.nom_candidat = nom_candidat && x.nom_formation = nom_formation -> 
-          [{rang_repondeur=rang_repondeur ;nom_candidat=nom_candidat ; nom_formation = nom_formation }]
-      | hd::tl -> 
-          [hd] @ aux tl
-        in aux session.voeu *)
-   
-(* let ajoute_voeu session ~rang_repondeur ~nom_candidat ~nom_formation = 
-      let rec aux (v: voeu list) = match v with 
-      | [] -> session.voeu <- [{rang_repondeur=rang_repondeur ;nom_candidat=nom_candidat ; nom_formation = nom_formation }]
-      | [x] when x.nom_candidat = nom_candidat && x.nom_formation = nom_formation -> 
-          session.voeu <- [{rang_repondeur=rang_repondeur ;nom_candidat=nom_candidat ; nom_formation = nom_formation }]
-      | hd::tl -> 
-          aux tl ;
-          session.voeu <- [hd] @ tl
-        in aux session.voeu  *)
+let renonce session ~nom_candidat ~nom_formation = 
+  ignore session;
+  ignore nom_candidat;
+  ignore nom_formation;
+  failwith "non implémenté"
 
+let consulte_propositions session ~nom_candidat =
+  ignore session;
+  ignore nom_candidat;
+  failwith "non implémenté"
 
-        
-(* let ajoute_voeu session ~rang_repondeur ~nom_candidat ~nom_formation = 
-  let rec aux (v: voeu list) = match v with 
-  | [] -> session.voeu <- [{rang_repondeur=rang_repondeur ;nom_candidat=nom_candidat ; nom_formation = nom_formation }]
-  | [x] when x.nom_candidat = nom_candidat && x.nom_formation = nom_formation && rang_repondeur > x.rang_repondeur -> 
-      session.voeu <- [{rang_repondeur=rang_repondeur ;nom_candidat=nom_candidat ; nom_formation = nom_formation }]
-  | hd::tl -> 
-      session.voeu <- session.voeu @ [hd] @ aux tl 
-    in aux session.voeu  *)
-
-    (* type session = char (* <- modifier *)
-
-    let nouvelle_session () = 
-      failwith "non implémenté"
-    
-    let ajoute_candidat session ~nom_candidat = 
-      ignore session;
-      ignore nom_candidat;
-      failwith "non implémenté"
-    
-    let ajoute_formation session ~nom_formation ~capacite =
-      ignore session;
-      ignore nom_formation;
-      ignore capacite;
-      failwith "non implémenté"
-    
-    let ajoute_voeu session ~rang_repondeur ~nom_candidat ~nom_formation = 
-      ignore session;
-      ignore rang_repondeur;
-      ignore nom_candidat;
-      ignore nom_formation;
-      failwith "non implémenté"
-    
-    let ajoute_commission session ~nom_formation ~fonction_comparaison = 
-      ignore session;
-      ignore nom_formation;
-      ignore fonction_comparaison;
-      failwith "non implémenté"
-    
-    let reunit_commissions session =
-      ignore session;
-      failwith "non implémenté"
-    
-    let nouveau_jour session =
-      ignore session;
-      failwith "non implémenté"
-    
-    let renonce session ~nom_candidat ~nom_formation = 
-      ignore session;
-      ignore nom_candidat;
-      ignore nom_formation;
-      failwith "non implémenté"
-    
-    let consulte_propositions session ~nom_candidat =
-      ignore session;
-      ignore nom_candidat;
-      failwith "non implémenté"
-    
-    let consulte_voeux_en_attente session ~nom_candidat = 
-      ignore session;
-      ignore nom_candidat;
-      failwith "non implémenté" *)
+let consulte_voeux_en_attente session ~nom_candidat = 
+  ignore session;
+  ignore nom_candidat;
+  failwith "non implémenté" 
